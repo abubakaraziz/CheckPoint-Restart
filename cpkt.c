b2bf977752b4cfd6b8396f8bbd5c7235fbe47e1c
AbuBakar <aziz313f@gmail.com>
2021-04-02 12:30:40 -0400

    Added Register Context and datasize

--- cpkt.c
+++ cpkt.c
@@ -5,37 +5,65 @@
 #include<signal.h>
 #include<stdlib.h>
 #include<unistd.h>
-
+#include "proc-self-maps.c" 
 int main(int,char *[]);
 sigjmp_buf env;
+int print_current_process_memory(struct proc_maps_line proc_maps[]){
+ printf(" *** Memory segments of Current process are :%s ***\n");   
+  int i = 0;
+  for (i = 0; i<1;/*proc_maps[i].start != NULL;*/ i++) {
+    printf("%s (%c%c%c) %d %d\n"
+           "  Address-range: %p - %p\n",
+           proc_maps[i].name,
+           proc_maps[i].rwxp[0], proc_maps[i].rwxp[1], proc_maps[i].rwxp[2],
+	   proc_maps[i].is_register_context,
+	   proc_maps[i].data_size,
+           proc_maps[i].start, proc_maps[i].end);
+  
+   //printf("%s \n",proc_maps[i].name);
+  }
+return 0;
+}
+
+int read_current_process_memory(struct proc_maps_line proc_maps[]){ 
+  assert(proc_self_maps(proc_maps)==0); 
+  
+return 0;
+}
+
+
 void signal_handler(int signal){
+
+   printf("Signal Received %d\n",signal);
+
+   if (signal==SIGUSR2){
+        printf("SIGUSR RECEVED \n"); 
+
+   }
 siglongjmp(env,1);
 }
 
 void __attribute__((constructor))
 my_constructor() {
-   signal(SIGINT,&signal_handler);
-   fprintf(stderr, "*************************************\n"
+	
+
+  struct proc_maps_line proc_maps[1000];
+ //Reading Memory Addresses  
+  read_current_process_memory(proc_maps);
+  print_current_process_memory(proc_maps);
+
+
+  /*
+  signal(SIGUSR2,&signal_handler);
+  fprintf(stderr, "*************************************\n"
                     "***  This is my CPKT ***\n"
                     "*************************************\n");
-   
-   
-  
+    
   if (sigsetjmp(env,1)!=0){
     printf("\n\n *** RESTARTING ***\n"); 
   }
 
-
-   
-   fprintf(stderr,   "*** What integer should we test? ");
-   fflush(stdout);
-   char buf[100]={'\0'}; //null characters at end of string
-   read(0,buf,sizeof(buf)-1);
-   char *argv[]={"UNKNOWN", "12345", NULL};
-   argv[0]=getenv("TARGET");
-   argv[1]=buf;
-   main(/* argc */2, argv);
-
-   exit(0);
+  */ 
+   //exit(0);
 
 }
