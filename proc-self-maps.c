c6cddcae7b9cd1fdbaf5bca975136ab0aeeb0454
AbuBakar <aziz313f@gmail.com>
2021-04-05 10:19:15 -0400

    Updated Proc-self-maps file

--- proc-self-maps.c
+++ proc-self-maps.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <assert.h>
 
 #define NAME_LEN 80
@@ -9,25 +12,31 @@ struct proc_maps_line {
   char rwxp[4];
   int read, write, execute; // Not used in this versoin of the code
   char name[NAME_LEN]; // for debutting only
-  int is_register_context; //for register context
+  int is_register_context; //for register context only
   int data_size;
 };
 
 // Returns 0 on success
-int match_one_line(FILE *proc_maps_file,
+int match_one_line(int proc_maps_fd,
                    struct proc_maps_line *proc_maps_line, char *filename) {
   unsigned long int start, end;
   char rwxp[4];
   char tmp[10];
-  int rc = fscanf(proc_maps_file, "%lx-%lx %4c %*s %*s %*[0-9 ]%[^\n]\n",
-                    &start, &end, rwxp, filename);
+  int tmp_stdin = dup(0); // Make a copy of stdin
+  dup2(proc_maps_fd, 0); // Duplicate proc_maps_fd to stain
+  // scanf() reads _only_ from stdin.  It's a dup of proc_maps_fd (same struct file).
+  int rc = scanf("%lx-%lx %4c %*s %*s %*[0-9 ]%[^\n]\n",
+                 &start, &end, rwxp, filename);
+  dup2(tmp_stdin, 0); // Restore original stdin; proc_maps_fd offset was advanced.
+  close(tmp_stdin);
   if (rc == EOF || rc == 0) { 
     proc_maps_line -> start = NULL;
     proc_maps_line -> end = NULL;
     return EOF;
   }
-  if (rc == 3) {
-    filename[0] = '\0';
+  if (rc == 3) { // if no filename on /proc/self/maps line:
+    strncpy(proc_maps_line -> name,
+            "ANONYMOUS_SEGMENT", strlen("ANONYMOUS_SEGMENT")+1);
   } else {
     assert( rc == 4 );
     strncpy(proc_maps_line -> name, filename, NAME_LEN-1);
@@ -36,32 +45,36 @@ int match_one_line(FILE *proc_maps_file,
   proc_maps_line -> start = (void *)start;
   proc_maps_line -> end = (void *)end;
   memcpy(proc_maps_line->rwxp, rwxp, 4);
-  proc_maps_line->is_register_context=0;
-  proc_maps_line->data_size=end-start;
   return 0;
 }
 
+// This might cause scanf to call mmap().  If this is a problem, call it a second
+//   time, and scanf() can re-use the previous memory, instead of mmap'ing more.
 int proc_self_maps(struct proc_maps_line proc_maps[]) {
   // NOTE:  fopen calls malloc() and potentially extends the heap segment.
-  FILE *proc_maps_file = fopen("/proc/self/maps", "r");
+  int proc_maps_fd = open("/proc/self/maps", O_RDONLY);
   char filename [100]; // for debugging
   int i = 0;
   int rc = -2; // any value that will not terminate the 'for' loop.
   for (i = 0; rc != EOF; i++) {
-    rc = match_one_line(proc_maps_file, &proc_maps[i], filename);
+    rc = match_one_line(proc_maps_fd, &proc_maps[i], filename);
 #ifdef DEBUG
     if (rc == 0) {
       printf("proc_self_maps: filename: %s\n", filename); // for debugging
     }
 #endif
   }
-  fclose(proc_maps_file);
+  close(proc_maps_fd);
   return 0;
 }
 
 #ifdef STANDALONE
 int main(int argc, char *argv[]) {
   struct proc_maps_line proc_maps[1000];
+  assert( proc_self_maps(proc_maps) == 0 );
+  // We purposely call proc_self_maps twice.  'scanf()' might call mmap()
+  //   during the first execution, above.  But it doesn't need to map
+  //   in a second memory segment after that.  It just re-uses the original one.
   assert( proc_self_maps(proc_maps) == 0 );
   printf("    *** Memory segments of %s ***\n", argv[0]);
   int i = 0;
