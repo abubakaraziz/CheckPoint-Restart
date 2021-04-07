d154e4e07bfaadfe18595d0db3f9d291621b1562
AbuBakar <aziz313f@gmail.com>
2021-04-06 21:37:42 -0400

    hello file added

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+
+ while(1){
+ printf(".\n");
+ fflush(stdout); 
+ sleep(1);
+
+ }
+
+
+}
